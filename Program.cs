576d1c0ad7af128f63ce4f3343e120ab9705eeb8
admin <admin@DESKTOP-6D3IIJS>
2021-10-06 16:50:27 +0300

    Am adaugat MyClass

--- Program.cs
+++ Program.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace FirstApp
 {
-    class Program
+    class MyClass
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hello word");
-        }
     }
 }
